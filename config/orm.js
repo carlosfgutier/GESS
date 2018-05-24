5ddf93936963094ac3914120e1654edf519bb03d
Carlos <carlosfgutier@gmail.com>
2018-05-24 15:00:14 -0500

    commit prior to change 05/24/2018

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,21 @@
 var connection = require("./connection.js");
 
+var orm = {
+
+//List of functions that take imput and conditions
+//and turns them into database queries. 
+
+//For example:
+
+	// 'Greet all' button triggers function that selects all names 
+	all: function(name, cb) {
+		var queryString = "SELECT * FROM" + name + ";";
+		connection.query(queryString, function(err, result) {
+
+		if (err) {
+			throw err;
+		} 	
+		cb(result);
+		});
+	};
+};
